30bfeb267f1969a3efd1e5aa6b3739bc555299ab
pravin-nawale <159033956+pravin-nawale@users.noreply.github.com>
2024-02-13 23:22:11 +0530

    Practice Practical

---
+++ 3 Validation/Validation/index.js
@@ -0,0 +1,10 @@
+function formValidation() {
+
+
+    var firstName = document.getElementById("firstName").value;
+
+    if (firstName === "" || firstName == NULL) {
+        alert("Please Enter Your Name");
+    }
+
+} formValidation()
